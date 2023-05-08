e72fc430c7605053fc3f376e511ad4225af62c8f
Luciano <lucianogpingindustrial@gmail.com>
2023-05-08 13:30:54 +0200

    init

--- UI/gui.py
+++ UI/gui.py
@@ -1,38 +1,10 @@
-from pathlib import Path
-from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+import tkinter as tk
+import pathlib as ph
+import engine
 
+root = tk.Tk()
 
-OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\luciano.galan\Desktop\Code\1_Repositories\Python_RyR\build\assets\frame0")
+frame = tk.Frame(root, width=500, height=300, bg='white')
+frame.pack()
 
-
-def relative_to_assets(path: str) -> Path:
-    return ASSETS_PATH / Path(path)
-
-
-window = Tk()
-
-window.geometry("728x488")
-window.configure(bg = "#7A7DCD")
-
-
-canvas = Canvas(
-    window,
-    bg = "#7A7DCD",
-    height = 488,
-    width = 728,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
-
-canvas.place(x = 0, y = 0)
-canvas.create_rectangle(
-    55.0,
-    46.0,
-    143.0,
-    91.0,
-    fill="#000000",
-    outline="")
-window.resizable(False, False)
-window.mainloop()
+root.mainloop()
