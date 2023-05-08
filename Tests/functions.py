bf97c1fdaf4a4db544dec16fa0e75af6093907a6
Luciano <lucianogpingindustrial@gmail.com>
2023-05-08 18:59:24 +0200

    dwda

--- Tests/functions.py
+++ Tests/functions.py
@@ -1,14 +1,19 @@
 import tkinter as tk
 import tkinter.filedialog
 
-def askdirectory():
+def askdirectory(text_field):
     source_dirname = tk.filedialog.askdirectory()
-    text1.delete(0, tk.END)
-    text1.insert(0, string)
+    text_field.delete(0, tk.END)
+    text_field.insert(0, source_dirname)
     return source_dirname
 
-def asktarget():
+def asktarget(text_field):
     target_dirname = tk.filedialog.askopenfilename()
-    text2.delete(0, tk.END)
-    text2.insert(0, string)
+    text_field.delete(0, tk.END)
+    text_field.insert(0, target_dirname)
     return target_dirname
+
+""" if selected_option == options[0]:
+    print("2")
+else:
+    print("4") """
--- Tests/main.py
+++ Tests/main.py
@@ -1,24 +1,34 @@
 import tkinter as tk
-import pathlib as ph
+import tkinter.filedialog
 from functions import askdirectory, asktarget
 
-#Root and frame creation
 root = tk.Tk()
-root.geometry("400x300")
-frame = tk.Frame(root, width=400, height=300, bg='white')
+
+# create a frame for the widgets
+frame = tk.Frame(root, width=300, height=200, bg='white')
 frame.pack()
 
-#Widgets placement
-button1 = tk.Button(frame, text='Select Reports', command=lambda: askdirectory())
+# create the button to select the reports folder and the associated text label
+button1 = tk.Button(frame, text='Button 1', command=lambda: askdirectory(text1))
 button1.grid(row=0, column=0, padx=5, pady=5)
 
 text1 = tk.Entry(frame)
-text1.grid(row=1, column=0, padx=5, pady=5)
+text1.grid(row=1, column=0, padx=50, pady=5)
 
-button2 = tk.Button(frame, text='Set Target File', command=lambda: asktarget(text2))
+# create the button to select the target file and the associated text label
+button2 = tk.Button(frame, text='Button 2', command=lambda: asktarget(text2))
 button2.grid(row=2, column=0, padx=5, pady=5)
 
 text2 = tk.Entry(frame)
-text2.grid(row=3, column=0, padx=5, pady=5)
+text2.grid(row=3, column=0, padx=50, pady=5)
+
+# create a drop-down list to select the number of nests
+options = ['Two nests', 'Four nests']
+selected_option = tk.StringVar()
+selected_option.set(options[0]) # set the default option
+combo_box = tk.OptionMenu(frame, selected_option, *options)
+combo_box.grid(row=4, column=0, padx=5, pady=5)
+
+
 
 root.mainloop()
