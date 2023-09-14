1a512d31cecae3e4f7a1472553ec0579c9aa4176
Luciano <lucianogpingindustrial@gmail.com>
2023-09-14 12:12:31 +0200

    a

--- Wire_Section_Calculator/src/_db_tools.py
+++ Wire_Section_Calculator/src/_db_tools.py
@@ -1,58 +1,77 @@
 import pandas as pd
 import sqlite3
+import os
 
 class dbHandler:
-    def __init__(self, source_name, db_name):
-        self.excel_path = "../references/" + source_name
+    def __init__(self, source_name, db_name, table_name):
+        self.source_path = "../references/" + source_name
         self.db_path = "../database/" + db_name
+        self.table_name = table_name
         self.df = None
         self.conn = None
-        self.table_name = None
-    def connect_from_excel(self):
-        try:
-            self.df = pd.read_excel(self.excel_path)
-            self.conn = sqlite3.connect(self.db_path)  # Will create the db if it doesn't exist
-        except Exception as e:
-            raise Exception(f"Error connecting to database: {str(e)}")
-    def create_table(self, table_name):
+    def table_from_source(self):
+        '''Creates a db connection and a table of the given name using data'''
+        extension = self.source_path.split(".")[-1]
+        match extension:
+            case "xlsx":
+                self._excel()
+            case "csv":
+                self._comma_separated_values()
+            case other:
+                raise Exception(f"Unsupported file format")
         try:
             if not self.conn:
                 raise Exception("Database connection is not established.")
-            # Validate the table_name here, e.g., check for valid characters
-            if not table_name.isalnum():
+            if not self.table_name.isalnum(): #Validate the table_name here, e.g., check for valid characters
                 raise Exception("Invalid table name.")
-            self.table_name = table_name
-            self.df.to_sql(table_name, self.conn, if_exists='replace', index=False)
-            print(f'Data from {self.excel_path} has been imported into the *{self.table_name}* table in {self.db_path}.')
+            self.table_name = self.table_name
+            self.df.to_sql(self.table_name, self.conn, if_exists='replace', index=False)
+            print(f'Data from {self.source_path} has been imported into the *{self.table_name}* table in {self.db_path}.')
+            return self.conn
         except Exception as e:
             raise Exception(f"Error creating table in database: {str(e)}")
-    def commit(self):
+    def add_table(self, table_name):
+        '''Adds a new table without touching the rest of the database'''
         try:
-            self.conn.commit()
+            if not self.conn:
+                raise Exception("Database connection is not established.")
+            if not table_name.isalnum():             
+                raise Exception("Invalid table name.")
+            self.df.to_sql(table_name, self.conn, if_exists='replace', index=False)
+            print(f'Data from {self.source_path} has been imported into the *{table_name}* table in {self.db_path}.')
         except Exception as e:
-            raise Exception(f"Error making a commit in {self.table_name}: {str(e)}")
-    def close(self):
+            raise Exception(f"Error creating table in database: {str(e)}")
+    def consult_tables(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+        for table in tables:
+            print(table[0])
+    def examine_table(self, table_name):
+        self.table_name = table_name
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT * FROM {table_name};")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    '''Internal methods'''
+    def _excel(self):
         try:
-            self.conn.close()
+            self.df = pd.read_excel(self.source_path)
+            self.conn = sqlite3.connect(self.db_path)  # Will create the db if it doesn't exist
         except Exception as e:
-            raise Exception(f"Error closing database connection: {str(e)}")
-    def create_cursor(self):
+            raise Exception(f"Error connecting to database: {str(e)}")
+    def _comma_separated_values(self):
         try:
-            self.cursor = self.conn.cursor()
-            return self.cursor
+            self.df = pd.read_csv(self.source_path)
+            self.conn = sqlite3.connect(self.db_path)  # Will create the db if it doesn't exist
         except Exception as e:
-            raise Exception(f"Error creating a cursor: {str(e)}")
-    def execute_query(self, query):
-        try:
-            if not self.cursor:
-                raise Exception("Cursor is not created.")
-            self.cursor.execute(query)
-        except Exception as e:
-            raise Exception(f"Error executing query: {str(e)}")
+            raise Exception(f"Error connecting to database: {str(e)}")
 
 #Test script
 if __name__ == '__main__':
-    dbh = dbHandler("awg_sections.xlsx", "lookup_table.db")
-    dbh.connect()
-    print(dbh.conn.tables)
-    dbh.close()
+    dbh = dbHandler("awg_sections.xlsx", "lookup_table.db", "awg")
+    dbh.table_from_source()
+    dbh.consult_tables()
+    dbh.examine_table('awg')
+
--- Wire_Section_Calculator/src/_main.py
+++ Wire_Section_Calculator/src/_main.py
@@ -1,33 +1,39 @@
-import math
-from _db_tools import dbHandler
+import sqlite3
+import os
 
 def get_section_by_area(S):
     # Query the database to fetch the wire section based on the calculated 'S'
-    dbh = dbHandler("awg_sections.xlsx", "lookup_table.db")
-    dbh.connect()
-    dbh.create_table("awg")
-    cursor = dbh.create_cursor()
-    query = f"SELECT section FROM section_table WHERE min_area <= {S} AND max_area >= {S}"
-    dbh.execute_query(query)
-    result = cursor.fetchone()
-    return result[0] if result else None
+    conn = sqlite3.connect("../database/lookup_table.db")
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM awg WHERE section >= {S};")
+    result = cursor.fetchall()
+    values = []
+    for row in result:
+        values.append(row)  
+        if values != []:
+            section = result[-1][1]
+            return section
+        else:
+            print("No matching values found.")
 
-def cc_current(length, intensity, material):
+def cc_current(length, intensity, vdrop, material):
     L = length #m
     I = intensity #A
+    dV = vdrop #%V
     match material:
         case "Cu":
             k = 56
         case "Al":
             k = 35
     
-    S = (2*L*I)/(k*0.01)
+    S = (2*L*I)/(k*dV)
     
+    print(f"Calculated section: {S} mm2")
     section = get_section_by_area(S)
-    
     print(f"The appropiate 'awg' for the required conditions is {section} mm2")
     
     return section
 
-section = cc_current(1, 1, "Cu")
+#Main script
+section = cc_current(1, 1, 0.01, "Cu") #Intensity, length, tension drop and material
 
