4fa1d7b033c324d8c84f60d9b2f5e47c92002d3d
Luciano <lucianogpingindustrial@gmail.com>
2023-09-14 13:21:42 +0200

    a

--- Wire_Section_Calculator/src/_db_tools.py
+++ Wire_Section_Calculator/src/_db_tools.py
@@ -1,52 +1,52 @@
 import pandas as pd
 import sqlite3
 import os
+#Secondary requirements: pip install openpyxl
 
-class dbHandler:
-    def __init__(self, source_name, db_name, table_name):
+class SQlite_Data_Extractor:
+    '''Extracts structured data from different sources and turn it into a table in a database for quick deployment'''
+    def __init__(self, source_name, db_name):
         self.source_path = "../references/" + source_name
         self.db_path = "../database/" + db_name
-        self.table_name = table_name
         self.df = None
         self.conn = None
+        if os.path.exists(self.db_path): #Preventive connection to an existing base
+            self.conn = sqlite3.connect(self.db_path)
+            self.cursor = self.conn.cursor()
     def table_from_source(self):
-        '''Creates a db connection and a table of the given name using data'''
-        extension = self.source_path.split(".")[-1]
+        '''Creates a db connection and a table of the given name using data for different sources'''
+        extension = self.source_path.split(".")[-1] #Gets the extension of the file
         match extension:
             case "xlsx":
-                self._excel()
+                try:
+                    self.df = pd.read_excel(self.source_path, sheet_name=None)
+                except Exception as e:
+                    raise Exception(f"Error importing data into pandas: {str(e)}")
+                self._datasheet()
             case "csv":
-                self._comma_separated_values()
+                try:
+                    self.df = pd.read_csv(self.source_path, sheet_name=None)
+                except Exception as e:
+                    raise Exception(f"Error importing data into pandas: {str(e)}")
+                self._datasheet()
             case other:
                 raise Exception(f"Unsupported file format")
-        try:
-            if not self.conn:
-                raise Exception("Database connection is not established.")
-            if not self.table_name.isalnum(): #Validate the table_name here, e.g., check for valid characters
-                raise Exception("Invalid table name.")
-            self.table_name = self.table_name
-            self.df.to_sql(self.table_name, self.conn, if_exists='replace', index=False)
-            print(f'Data from {self.source_path} has been imported into the *{self.table_name}* table in {self.db_path}.')
-            return self.conn
-        except Exception as e:
-            raise Exception(f"Error creating table in database: {str(e)}")
     def add_table(self, table_name):
         '''Adds a new table without touching the rest of the database'''
         try:
             if not self.conn:
                 raise Exception("Database connection is not established.")
-            if not table_name.isalnum():             
-                raise Exception("Invalid table name.")
-            self.df.to_sql(table_name, self.conn, if_exists='replace', index=False)
             print(f'Data from {self.source_path} has been imported into the *{table_name}* table in {self.db_path}.')
         except Exception as e:
             raise Exception(f"Error creating table in database: {str(e)}")
+    def rename_table(self, og_table_name):
+        ...
     def consult_tables(self):
         cursor = self.conn.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         tables = cursor.fetchall()
         for table in tables:
-            print(table[0])
+            print(table)
     def examine_table(self, table_name):
         self.table_name = table_name
         cursor = self.conn.cursor()
@@ -54,24 +54,54 @@ class dbHandler:
         rows = cursor.fetchall()
         for row in rows:
             print(row)
-    '''Internal methods'''
-    def _excel(self):
+    def close_conn(self):
+        self.conn.close()  #Close the database connection when done
+    def clear_database(self, db_name):
+        self.db_path = "../database/" + db_name
         try:
-            self.df = pd.read_excel(self.source_path)
-            self.conn = sqlite3.connect(self.db_path)  # Will create the db if it doesn't exist
+            conn = sqlite3.connect(self.db_path)
+            cursor = conn.cursor()
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';") #Get a list of all tables in the database
+            tables = cursor.fetchall()
+            print("Warning: This action will clear all data from the database.")
+            confirmation = input("Do you want to continue? (y/n): ").strip().lower()
+            if confirmation == 'y':
+                # Loop through the tables and delete them
+                for table in tables:
+                    table_name = table[0]
+                    cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+                conn.commit()
+                conn.close()
+                print("Database cleared successfully.")
+            else:
+                print("Operation canceled.")
         except Exception as e:
-            raise Exception(f"Error connecting to database: {str(e)}")
-    def _comma_separated_values(self):
+            print(f"Error clearing the database: {str(e)}")
+    '''Internal methods'''
+    def _datasheet(self):
         try:
-            self.df = pd.read_csv(self.source_path)
             self.conn = sqlite3.connect(self.db_path)  # Will create the db if it doesn't exist
+            print(f'Data from {self.source_path} has been imported in {self.db_path}.')
+            print(f"Sheet(s) imported to db as table(s) with name(s):")
+            for sheet_name, sheet in self.df.items():
+                print(f"{sheet_name}s")
+                if not sheet_name.isalnum():
+                    raise Exception(f"Invalid table name for sheet: {sheet_name}")
+                table_name = sheet_name
+                sheet.to_sql(table_name, self.conn, if_exists='replace', index=False)
         except Exception as e:
             raise Exception(f"Error connecting to database: {str(e)}")
 
 #Test script
 if __name__ == '__main__':
-    dbh = dbHandler("awg_sections.xlsx", "lookup_table.db", "awg")
+    dbh = SQlite_Data_Extractor("awg_sections.xlsx", "lookup_table.db")
     dbh.table_from_source()
     dbh.consult_tables()
-    dbh.examine_table('awg')
+    dbh.examine_table("test")
+    dbh.close_conn()
+    
+    ###WARNING###
+    dbh.clear_database("lookup_table.db")
+
+
 
