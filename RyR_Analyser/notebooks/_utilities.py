9ce7f21c924ea637bdeaac2c57deb952d0b1f235
Luciano <lucianogpingindustrial@gmail.com>
2023-09-26 17:19:51 +0200

    a

--- RyR_Analyser/notebooks/_utilities.py
+++ RyR_Analyser/notebooks/_utilities.py
@@ -4,20 +4,23 @@ import sys
 sys.path.append("../tools/")
 import _db_tools as db
 
+#Helper functions
 def prepare_data(target):
+    '''Prepares the output data by extracting the measures in a df and storing them in the database'''
     data = pd.read_excel(target) #Import the RyR generator output
     df = data.iloc[2:, 1:-2] #Slices the measures
     df.reset_index(drop=True, inplace=True) #Reset rows index
     df.columns = range(df.shape[1]) #Reset columns index
-    dbh.store_df(df, "measures") #Store the dataframe in the connected database
+    return df
+
+def prepare_database(df, table_name):
+    '''Prepares the database from a ready-to-store df and returns the same df, getting it from the db'''
+    dbh = db.SQLite_Data_Extractor("database.db") #Allows for database management
+    dbh.store_df(df, table_name) #Store the dataframe in the connected database
     dbh.consult_tables() #Checks results
+    dbh.close_conn()
 
 #test script
 if __name__ == "__main__":
-    dbh = db.SQLite_Data_Extractor("database.db")
     prepare_data("../data/target.xlsx")
 
-
-test = [0, 1, 2]
-
-test = [test]
--- RyR_Analyser/notebooks/test.py
+++ RyR_Analyser/notebooks/test.py
@@ -1,38 +1,36 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import sys
-sys.path.append("../tools/")
-import _db_tools as db
+import configparser
+import pandas as pd
 
-dbh = db.SQLite_Data_Extractor("database.db")
-df = dbh.retrieve("measures")
+limits = pd.DataFrame([[0.324, 0.3541, 0.333, 0.3572, 0.3452, 0.3707, 0.3247, 0.3519, 0.3322, 0.3586, 0.3365, 0.3656], [0.3248, 0.3538, 0.3302, 0.3588, 0.3435, 0.3688, 0.3324, 0.3593, 0.3385, 0.3623, 0.3512, 0.3728]]).transpose()
+print(limits)
 
-# Assuming you have defined fibers, df, and legend_label earlier in your code
+##Creation of the .ini
+#Create a ConfigParser instance and read the .ini file
+config = configparser.ConfigParser()
+config.read('../data/template.ini')
 
-# Define a list of colormaps (one for each scatter plot)
-colormaps = ['viridis', 'coolwarm', 'plasma', 'inferno']  # You can choose your colormaps
+#Iterate through the sections and options in the .ini file
+HI_LIMIT = limits.iloc[:, 1]
+LO_LIMIT = limits.iloc[:, 0]
+for section in config.sections():
+    keys_list = list(config[section].keys())
+    j = 0
+    for i in range(0, len(keys_list), 2):
+        key1 = keys_list[i]
+        key2 = keys_list[i + 1]
 
-fibers = [1, 2, 3, 4]
+        col1 = str(limits.iloc[j, 1])
+        col2 = str(limits.iloc[j, 0])
+        j += 1
+        config[section][key1] = col1
+        config[section][key2] = col2
 
-# Loop through the fibers and create scatter plots with different colormaps
-for i, fiber in enumerate(fibers):
-    x_values = list(element + 1 for element in range(df.shape[1]))
-    y_values = df.iloc[fiber - 1]
-    
-    # Use a different colormap for each scatter plot
-    plt.scatter(
-        x_values,
-        y_values,
-        cmap=colormaps[i % len(colormaps)]  # Cycle through the list of colormaps
-    )
+#Loop through the sections and options in the .ini file and print them
+for section in config.sections():
+    print(f"[{section}]")
+    for key, value in config.items(section):
+        print(f"{key} = {value}")
 
-# Add a colorbar to the plot (optional)
-plt.colorbar(label='Colorbar Label')
-
-# Add labels, legends, and other plot decorations as needed
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.legend()
-
-# Show the plot
-plt.show()
+#Save the modified data to a new .ini file
+with open('../output/limits.ini', 'w') as configfile:
+    config.write(configfile)
