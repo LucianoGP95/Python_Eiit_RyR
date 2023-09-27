322965f29806789c7c7a0290f382368343eda7b5
Luciano <lucianogpingindustrial@gmail.com>
2023-09-27 09:21:47 +0200

    a

--- RyR_Analyser/notebooks/test.py
+++ RyR_Analyser/notebooks/test.py
@@ -1,16 +1,5 @@
 import configparser
 
-class CaseSensitiveConfigParser(configparser.ConfigParser):
-    def optionxform(self, optionstr):
-        return optionstr
-config = CaseSensitiveConfigParser()
-config.read('../data/template.ini')
-keys_list = []
-
-# Iterate through sections and add keys to the list
-for section_name in config.sections():
-    section = config[section_name]
-    keys_list.extend(section.keys())
-
-# Now, keys_list contains all the keys from the INI file, preserving case
-print(keys_list)
+for j in range(3):  # This outer loop controls the 'j' iteration
+    for i in range(2):  # This inner loop controls the 'i' iteration, iterating twice for each 'j'
+        print(f"j={j}, i={i}")
