cd9270e2a36806015fc221cfd5d7fa41e600beb4
Luciano <lucianogpingindustrial@gmail.com>
2023-09-30 10:51:32 +0200

    a

--- RyR_Analyser/notebooks/__globals.py
+++ RyR_Analyser/notebooks/__globals.py
@@ -1,6 +1,17 @@
+from dataclasses import dataclass
 ####Global variables####
-tooling = "PASSAT_B9"
-leds_per_nest = 3
-tolerance = 0.015
-lo_tolerance = 0.02
-hi_tolerance = 0.03
+@dataclass
+class Globals:
+    tooling: str = "PASSAT_B9"
+    leds_per_nest: int = 3
+    tolerance: float = 0.015
+    lo_tolerance: float = 0.02
+    hi_tolerance: float = 0.03
+
+glob = Globals(
+    tooling="PASSAT_B9",
+    leds_per_nest=4,
+    tolerance=0.015,
+    lo_tolerance=0.02,
+    hi_tolerance=0.03
+)
