9c5bd159b9408630eb454521cded8bd452440508
Luciano <lucianogpingindustrial@gmail.com>
2023-11-08 21:58:36 +0100

    a

--- a2_RyR_Analyser/notebooks/globals.py
+++ a2_RyR_Analyser/notebooks/globals.py
@@ -4,15 +4,15 @@ from dataclasses import dataclass
 class Globals:
     tooling: str = "" #Refcode name of the tooling
     lenses_per_nest: int = 3 #Lenses placed in each nest
-    x_tolerance: float = 0.0125 #X colour value allowed tolerance
-    y_tolerance: float = 0.015 #Y colour value allowed tolerance
+    x_tolerance: float = 0.0125 #X colour value allowed tolerance from nominal to limit
+    y_tolerance: float = 0.015 #Y colour value allowed tolerance from nominal to limit
     lo_tolerance: float = 0.02 #General lower limit allowed tolerance
     hi_tolerance: float = 0.03 #General upper limit allowed tolerance
 
 glob = Globals(
-    tooling="Test",
+    tooling="TOP_Passat_B9",
     lenses_per_nest=3,
-    x_tolerance=0.0125,
+    x_tolerance=0.0125, 
     y_tolerance=0.015,
     lo_tolerance=0.02,
     hi_tolerance=0.03
