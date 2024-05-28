7c9c72387bc949d0d4d473d444da2726c5f77ac0
Luciano <luciano.galan@eiit.com>
2024-05-28 14:40:48 +0200

    a

--- a2_Analyser/_prepare_software.py
+++ a2_Analyser/_prepare_software.py
@@ -1,17 +1,31 @@
-### Script to quickly set desired software into a dektop folder.
+### Script to quickly set the parent folder software into a desktop folder.
 ### Run after cloning the repository in your PC.
 
 import os
 import shutil
 import getpass
+import subprocess
 
 def copytree_overwrite(source, target):
     '''Wrapper function to delete the destination folder and copy the desired files over it'''
     try:
         if os.path.exists(target):
+            print("Copying files")
             shutil.rmtree(target)
         shutil.copytree(source, target)
         os.remove(os.path.join(target, "_prepare_software.py"))
+        return True
+    except OSError as e:
+        print(f"Error: {e}")
+        return False
+
+def create_venv(target):
+    '''Tries to execute a PowerShell script to create a Python env'''
+    script_path = os.path.join("C:\\Users", getpass.getuser(), "Powershell", "Scripts", "PythonVenv.ps1")
+    try:
+        os.chdir(target)
+        print("Creating venv")
+        subprocess.run(["powershell.exe", "-File", script_path])
         return True
     except OSError as e:
         print(f"Error: {e}")
@@ -27,21 +41,10 @@ def main():
     else:
         print("Error: Unable to copy files.")
 
-    if create_venv():
+    if create_venv(target):
         print(f"Success: Venv created in {target}")
     else:
         print("Error: Unable to create venv.")
-
-def create_venv():
-    '''Tries to execute a PowerShell script to create a Python env'''
-    script_path = os.path.join("C:\\Users", getpass.getuser(), "Powershell", "Scripts", "PythonVenv.ps1")
-    try:
-        exec(open(script_path).read())
-        print("Creating venv")
-        return True
-    except OSError as e:
-        print(f"Error: {e}")
-        return False
 
 if __name__ == "__main__":
     main()
