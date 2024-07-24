10048c93db0abca10034d65db9f6fb57c724f727
Luciano <luciano.galan@eiit.com>
2024-07-24 15:19:55 +0200

    a

---
+++ a1_RyR_Generator/v15.0/Run_app.ps1
@@ -0,0 +1,4 @@
+#Start-Process -FilePath "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoNewWindow
+C:\Users\User\Desktop\RyR_Generator\.venv\Scripts\Activate.ps1
+Set-Location C:\Users\User\Desktop\RyR_Generator\app\src\
+python main.py
