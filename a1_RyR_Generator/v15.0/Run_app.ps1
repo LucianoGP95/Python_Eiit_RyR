9a33e08200428a15ed766f973d52e1fd9b615b58
Luciano <luciano.galan@eiit.com>
2024-08-09 12:25:00 +0200

    a

--- a1_RyR_Generator/v15.0/Run_app.ps1
+++ a1_RyR_Generator/v15.0/Run_app.ps1
@@ -1,4 +1,16 @@
-#Start-Process -FilePath "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoNewWindow
-C:\Users\User\Desktop\RyR_Generator\.venv\Scripts\Activate.ps1
-Set-Location C:\Users\User\Desktop\RyR_Generator\app\src\
+# Start PowerShell in the current window (optional, not necessary for most scripts)
+# Start-Process -FilePath "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -NoNewWindow
+
+# Construct paths
+$env_directory = Join-Path $PSScriptRoot ".venv\Scripts\Activate.ps1"
+$src_directory = Join-Path $PSScriptRoot "app\src"
+
+# Activate the virtual environment
+& $env_directory
+Write-Host "env succesfully activated: $env_directory" -ForegroundColor Green
+
+# Change directory to the source directory
+Set-Location $src_directory
+
+# Run the main.py script
 python main.py
